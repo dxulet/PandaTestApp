648db4f91e8cad08c241498f8101130f5bdde131
Daulet Ashikbayev <dxulet@gmail.com>
2024-03-06 00:27:22 +0500

    fix landscape bug

--- PandaTest/App/Details/DetailsView.swift
+++ PandaTest/App/Details/DetailsView.swift
@@ -11,30 +11,32 @@ struct DetailsView: View {
     @Binding var isPresented: Bool
     
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            Image(Constants.pandaImage)
-                .resizable()
-                .scaledToFill()
-                .frame(width: UIScreen.main.bounds.width,
-                       height: UIScreen.main.bounds.height,
-                       alignment: .leading)
-                .ignoresSafeArea()
-            
-            Button(action: {
-                withAnimation(.easeInOut) {
-                    isPresented.toggle()
-                }
+        GeometryReader { geo in
+            ZStack(alignment: .topTrailing) {
+                Image(Constants.pandaImage)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geo.size.width,
+                           height: geo.size.height,
+                           alignment: .leading)
                 
-            }) {
-                Image(systemName: Constants.closeButtonImage)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: Constants.closeButtonSize)
-                    .background(Circle().fill(Color.gray))
+                Button(action: {
+                    withAnimation(.easeInOut) {
+                        isPresented.toggle()
+                    }
+                    
+                }) {
+                    Image(systemName: Constants.closeButtonImage)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: Constants.closeButtonSize)
+                        .background(Circle().fill(Color.gray))
+                }
+                .padding(.trailing, 20)
+                .padding(.top, 40)
             }
-            .padding(.trailing, 20)
-            .padding(.top, 40)
         }
+        .ignoresSafeArea()
         .transition(.move(edge: .top))
     }
 }
